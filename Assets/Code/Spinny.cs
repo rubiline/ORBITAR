0c9f6a68e0542b03b55d3eafd04c54e7479cf66c
nil <nillinvanillin@gmail.com>
2023-09-24 19:39:47 -0400

    AND THIS

---
+++ Assets/Code/Spinny.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinny : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles += new Vector3(0, 0, speed * Time.deltaTime);
+    }
+}
