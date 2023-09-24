850ec2494f42fe02028ddc181c4f98579e215e80
Ruby Tartakovsky <rubilineroundabout@gmail.com>
2023-09-24 18:20:36 -0400

    cutscene

--- Assets/CutsceneManager.cs
+++ Assets/CutsceneManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,30 +10,66 @@ public class CutsceneManager : MonoBehaviour
     public List<Cutscene> CutsceneList;
     public SpriteRenderer SlideshowHost;
 
-    private int _index;
+    public List<DialogueTextBox> DialogueBoxList;
+
+    public GameObject WaitPrompt;
+
+    private int _imageIndex;
+    private int _textIndex;
+    private int _animationIndex;
     private Cutscene _currentCutscene;
+    private bool waiting = false;
     // Start is called before the first frame update
     void Start()
     {
-        _index = 0;
+
         PlayCutscene(GameManager.Instance.CurrentCutscene);
+    }
+
+    private void Update()
+    {
+        if (waiting && Input.GetKeyUp(KeyCode.Z)) 
+        {
+            waiting = false;
+            Director.Resume();
+            WaitPrompt.SetActive(false);
+        }
     }
 
     void PlayCutscene(string cutsceneName)
     {
+        _imageIndex = 0;
+        _textIndex = 0;
+        _animationIndex = 0;
+
         Cutscene c = CutsceneList.Find(a => a.ID == cutsceneName);
         if (c != null)
         {
+            if (c.ShowBox1) ShowBox(DialogueBoxList[0]);
+            if (c.ShowBox2) ShowBox(DialogueBoxList[1]);
+            if (c.ShowBox3) ShowBox(DialogueBoxList[2]);
+
             _currentCutscene = Instantiate(c);
             Director.playableAsset = _currentCutscene.Timing;
             Director.Play();
         }
     }
 
+    public void CloseAllBoxes()
+    {
+        foreach (DialogueTextBox box in DialogueBoxList)
+        {
+            HideBox(box);
+        }
+    }
+
     public void Advance()
     {
-        SlideshowHost.sprite = _currentCutscene.Slideshow[_index];
-        _index++;
+        if (_imageIndex < _currentCutscene.Slideshow.Count)
+        {
+            SlideshowHost.sprite = _currentCutscene.Slideshow[_imageIndex];
+            _imageIndex++;
+        }
     }
 
     public void FadeIn()
@@ -61,4 +98,56 @@ public class CutsceneManager : MonoBehaviour
         }
     }
 
+    public void AdvanceText()
+    {
+        if (_textIndex < _currentCutscene.Text.Count)
+        {
+            Dialogue d = _currentCutscene.Text[_textIndex];
+
+            DialogueBoxList.ForEach(a => a.ClearText());
+            DialogueBoxList[d.Box].SetText(d.Text);
+        }
+
+        _textIndex++;
+    }
+
+    public void AdvanceAnimation()
+    {
+        if (_animationIndex < _currentCutscene.Animations.Count)
+        {
+            CutsceneAnimation d = _currentCutscene.Animations[_animationIndex];
+
+            StartCoroutine(SpawnAnimation(d));
+        }
+
+        _animationIndex++;
+    }
+
+    IEnumerator SpawnAnimation(CutsceneAnimation animation) {
+        GameObject g = Instantiate(animation.Prefab, animation.Location, Quaternion.identity);
+
+        yield return new WaitForSeconds(animation.Time);
+
+        Destroy(g);
+    }
+
+    public void WaitForInput()
+    {
+        if (!waiting)
+        {
+            waiting= true;
+            Director.Pause();
+            WaitPrompt.SetActive(true);
+        }
+    }
+
+    private void ShowBox(DialogueTextBox box)
+    {
+        box.gameObject.SetActive(true);
+    }
+
+    private void HideBox(DialogueTextBox box)
+    {
+        box.gameObject.SetActive(false);
+    }
 }
---
+++ Assets/DialogueTextBox.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueTextBox
+    : MonoBehaviour
+{
+    private int index;
+    private string actualText = "";
+    public string targetText;
+
+    public TextMeshPro logTextBox;
+
+
+    public void SetText(string tgt)
+    {
+        index = 0;
+        actualText = "";
+        targetText = tgt;
+        ReproduceText();
+    }
+
+    public void ClearText()
+    {
+        actualText = "";
+        logTextBox.text = "";
+    }
+
+
+    private void ReproduceText()
+    {
+        //if not readied all letters
+        if (index < targetText.Length)
+        {
+            //get one letter
+            char letter = targetText[index];
+
+            //Actualize on screen
+            logTextBox.text = Write(letter);
+
+            //set to go to the next
+            index += 1;
+            StartCoroutine(PauseBetweenChars(letter));
+        }
+    }
+
+    private string Write(char letter)
+    {
+        actualText += letter;
+        return actualText;
+    }
+
+    private IEnumerator PauseBetweenChars(char letter)
+    {
+        switch (letter)
+        {
+            case '.':
+                yield return new WaitForSeconds(0.2f);
+                ReproduceText();
+                yield break;
+            case ',':
+                yield return new WaitForSeconds(0.2f);
+                ReproduceText();
+                yield break;
+            case ' ':
+                yield return new WaitForSeconds(0.1f);
+                ReproduceText();
+                yield break;
+            default:
+                yield return new WaitForSeconds(0.05f);
+                ReproduceText();
+                yield break;
+        }
+    }
+}
