e11185738f6632033a9e9d319257bb1e76f4e430
Ruby Tartakovsky <rubilineroundabout@gmail.com>
2023-09-25 01:10:07 -0400

    music and food

---
+++ Assets/Cred.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cred
+    : MonoBehaviour
+{
+
+    public Transform Credits;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return)) {
+            LoadTitle();
+        }
+
+        if (Credits.position.y > 26)
+        {
+            LoadTitle();
+        }
+    }
+
+    void LoadTitle()
+    {
+        GameManager.Instance.LoadScene("Title");
+    }
+}
